10a192511b59c4bf2e767aad1f4f0e5941029954
kempz <zoe.kemp@capgemini.com>
2021-11-15 10:55:17 +0000

    Create routing.js

---
+++ app/routes/iteration12/routing.js
@@ -0,0 +1,19 @@
+module.exports = function (router) {
+var version = "iteration12";
+
+
+    router.post('/' + version + '/installer/subcontractor', function (req, res) {
+          var installer_type = req.session.data['installer_type']
+          if (installer_type == "installer") {
+          res.redirect(301, '/' + version + '/installer/subcontractor')
+          } else {
+          res.redirect(301, '/' + version + '/installer/confirmSubcontractor')
+          }
+
+        })
+        router.post('/' + version + '/installer/dateOfCompletion', function (req, res) {
+                res.redirect(301, '/' + version + '/installer/chargepoint-model')
+        })
+
+
+}
