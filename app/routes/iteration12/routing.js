358bdf294b3407e296330c3957b3dda6853e98e0
kempz <zoe.kemp@capgemini.com>
2021-12-07 15:23:15 +0000

    parking - routing

--- app/routes/iteration12/routing.js
+++ app/routes/iteration12/routing.js
@@ -16,47 +16,55 @@ router.post('/' + version + '/landlordReg/type-of-landlord', function (req, res)
             res.redirect(301, '/' + version + '/landlordReg/linked-enterprise')
             } else {
             res.redirect(301, '/' + version + '/landlordReg/company-identification')
-      }})
+    }})
 
 router.post('/' + version + '/landlordReg/linked-enterprise', function (req, res){
-                  var landlord_type = req.session.data['linked_type']
-                  if (landlord_type == "yes") {
-                  res.redirect(301, '/' + version + '/landlordReg/id-linked-enterprise')
-                  } else {
-                  res.redirect(301, '/' + version + '/landlordReg/company-identification')
-      }})
+            var landlord_type = req.session.data['linked_type']
+            if (landlord_type == "yes") {
+            res.redirect(301, '/' + version + '/landlordReg/id-linked-enterprise')
+            } else {
+            res.redirect(301, '/' + version + '/landlordReg/company-identification')
+    }})
 
 router.post('/' + version + '/pmcApp/type-of-landlord', function (req, res){
-                        var pmc_landlord_type = req.session.data['pmc_landlord_type']
-                        if (pmc_landlord_type == "landlord") {
-                        res.redirect(301, '/' + version + '/pmcApp/linked-enterprise')
-                        } else {
-                        res.redirect(301, '/' + version + '/pmcApp/company-identification')
-      }})
+            var pmc_landlord_type = req.session.data['pmc_landlord_type']
+            if (pmc_landlord_type == "landlord") {
+            res.redirect(301, '/' + version + '/pmcApp/linked-enterprise')
+            } else {
+            res.redirect(301, '/' + version + '/pmcApp/company-identification')
+    }})
 
 router.post('/' + version + '/pmcApp/linked-enterprise', function (req, res){
-                        var landlord_type = req.session.data['linked_type']
-                        if (landlord_type == "yes") {
-                        res.redirect(301, '/' + version + '/pmcApp/id-linked-enterprise')
-                        } else {
-                        res.redirect(301, '/' + version + '/pmcApp/company-identification')
-      }})
+            var landlord_type = req.session.data['linked_type']
+            if (landlord_type == "yes") {
+            res.redirect(301, '/' + version + '/pmcApp/id-linked-enterprise')
+            } else {
+            res.redirect(301, '/' + version + '/pmcApp/company-identification')
+    }})
 
 router.post('/' + version + '/landlordReg/account-type', function (req, res){
-                        var business_type = req.session.data['business_type']
-                        if (business_type == "landlord") {
-                        res.redirect(301, '/' + version + '/landlordReg/email')
-                        } else {
-                        res.redirect(301, '/' + version + '/pmcReg/email')
+            var business_type = req.session.data['business_type']
+            if (business_type == "landlord") {
+            res.redirect(301, '/' + version + '/landlordReg/email')
+            } else {
+            res.redirect(301, '/' + version + '/pmcReg/email')
     }})
 
 router.post('/' + version + '/pmcReg/account-type', function (req, res){
-                            var business_type = req.session.data['business_type']
-                            if (business_type == "pmc") {
-                            res.redirect(301, '/' + version + '/pmcReg/email')
-                            } else {
-                            res.redirect(301, '/' + version + '/landlordReg/email')
-        }})
+            var business_type = req.session.data['business_type']
+            if (business_type == "pmc") {
+            res.redirect(301, '/' + version + '/pmcReg/email')
+            } else {
+            res.redirect(301, '/' + version + '/landlordReg/email')
+   }})
+
+router.post('/' + version + '/landlordApp/off-street-parking', function (req, res){
+            var parking = req.session.data['parking']
+            if (parking == "yes") {
+            res.redirect(301, '/' + version + '/landlordApp/declaration')
+            } else {
+            res.redirect(301, '/' + version + '/landlordapp/cannot-proceed')
+  }})
 
 
     router.post('/' + version + '/installer/dateOfCompletion', function (req, res) {
