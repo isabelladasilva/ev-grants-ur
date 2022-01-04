56b2849b1d288d09f5c28d030c48f336b32d49bd
shaleensachdeva <94457258+shaleensachdeva@users.noreply.github.com>
2022-01-04 17:10:26 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 require('./routes/iteration12/routing.js')(router);
-
 require('./routes/HC/iteration10/routing.js')(router);
 
 module.exports = router
