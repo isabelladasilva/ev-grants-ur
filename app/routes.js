805b8a3364de67c186f6146d0f9f0de1aba8e348
shaleensachdeva <94457258+shaleensachdeva@users.noreply.github.com>
2022-02-21 18:42:44 +0000

    again

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-require('./routes/iteration12/routing.js')(router);
-// require('./routes/iteration13/routing.js')(router);
-//require('./routes/mvp/routing.js')(router);
-//require('./routes/mmvp/routing.js')(router);
+// require('./routes/iteration12/routing.js')(router);
+
 
 require('./routes/HC/iteration10/routing.js')(router);
-require('./routes/HC/iteration11/routing.js')(router);
+
 
 require('./routes/CarPark/iteration01/routing.js')(router);
 
