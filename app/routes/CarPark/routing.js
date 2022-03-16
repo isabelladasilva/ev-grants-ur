082aa8bab5d7bff3bc0ff4d01cc36385f11ae86e
shaleensachdeva <94457258+shaleensachdeva@users.noreply.github.com>
2022-03-16 09:43:22 +0000

    s

--- app/routes/CarPark/routing.js
+++ app/routes/CarPark/routing.js
@@ -4,12 +4,12 @@ module.exports = function (router) {
   var apV = "01";
   var appVersion = "iteration"+apV;
   var installerModule = "installer";
-  //var installerModule2 = "installer-2";
+  var installerModule2 = "installer-2";
 
 
 
   var installerPath = '/' + appName + '/' + appVersion + '/' + installerModule;
-  //var installerPath = '/' + appName + '/' + appVersion + '/' + installerModule2;
+  var installerPath = '/' + appName + '/' + appVersion + '/' + installerModule2;
   //var installerClaimGrant = installerPath + '/claim-grant';//
 
 
@@ -25,13 +25,13 @@ module.exports = function (router) {
 
 
   //Who completed the job - company or sub-subcontractor for claiming a grant journey
-  //router.post('/installation-job', function (req, res)
-  //{
-  //  if (req.session.data['installer_type'] == "installer") {
-  //    res.redirect(installerPath+'/date-of-completion');
-  //  } else {
-  //    res.redirect(installerPath+'/confirm-subcontractor');
-  //  }
-  //})
+  router.post('/installation-job', function (req, res)
+  {
+    if (req.session.data['installer_type'] == "installer") {
+      res.redirect(installerPath+'/date-of-completion');
+    } else {
+      res.redirect(installerPath+'/confirm-subcontractor');
+    }
+  })
 
 }
