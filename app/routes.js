f95d5fa5c7594dbd0403ca18cd3ca17f6c7ea429
shaleensachdeva <94457258+shaleensachdeva@users.noreply.github.com>
2022-03-16 09:38:25 +0000

    for ur

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-require('./routes/CarPark/iteration01/routing.js')(router);
-
 require('./routes/iteration12/routing.js')(router);
+require('./routes/iteration13/routing.js')(router);
+require('./routes/mvp/routing.js')(router);
 require('./routes/mmvp/routing.js')(router);
+
+require('./routes/HC/iteration10/routing.js')(router);
+require('./routes/HC/iteration11/routing.js')(router);
+require('./routes/HC/iteration12/routing.js')(router);
+
+//require('./routes/CarPark/iteration01/routing.js')(router);
+require('./routes/CarPark/iteration02/routing.js')(router);
 
 module.exports = router
---
+++ app/routes/CarPark/iteration02/routing.js
@@ -0,0 +1,26 @@
+module.exports = function (router) {
+
+  var appName = "CarPark";
+  var apV = "02";
+  var appVersion = "iteration"+apV;
+  var installerModule2 = "installer-2";
+
+
+
+
+  var installerPath = '/' + appName + '/' + appVersion + '/' + installerModule2;
+  //var installerClaimGrant = installerPath + '/claim-grant';//
+
+
+  //Who completed the job - company or sub-subcontractor for claiming a grant journey
+  router.post('/installation-job', function (req, res)
+  {
+    if (req.session.data['installer_type'] == "installer") {
+      res.redirect(installerPath+'/date-of-completion');
+    } else {
+      res.redirect(installerPath+'/confirm-subcontractor');
+    }
+  })
+
+
+}
