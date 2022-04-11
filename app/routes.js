a9d24c7582cc62a6bb024fcac0c0fe2e4c14962b
shaleensachdeva <94457258+shaleensachdeva@users.noreply.github.com>
2022-04-11 16:14:27 +0100

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 require('./routes/iteration12/routing.js')(router);
-//require('./routes/iteration13/routing.js')(router);
-//require('./routes/mvp/routing.js')(router);
+require('./routes/iteration13/routing.js')(router);
+require('./routes/mvp/routing.js')(router);
 require('./routes/mmvp/routing.js')(router);
 
 require('./routes/HC/iteration10/routing.js')(router);
+require('./routes/HC/iteration11/routing.js')(router);
+require('./routes/HC/iteration12/routing.js')(router);
 
+//require('./routes/CarPark/iteration01/routing.js')(router);
 require('./routes/CarPark/iteration02/routing.js')(router);
+require('./routes/CarPark/iteration03/routing.js')(router);
 
 module.exports = router
